72aa2c23d98440a7f73310dca69429e525713c75
Sayed Ahad Karim <xhxd.kxrxm@gmail.com>
2022-03-31 21:59:50 +0600

    Add files via upload

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+import logo from "./netflixLogo.png";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src={logo} alt="Netflix Logo" />
+
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
+//2:43:28
